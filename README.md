ace5490c9a621cd5f8ecadbdfb7c418d6f31caf6
Biringa Chidera <biringachidera@gmail.com>
2021-11-08 10:12:28 -0500

    added setup and datasets

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpecDet
+SpecDet: A Transformer Neural Architecture for Detecting Spectre Attacks Using CPU Processes States and Spectre Gadgets
---
+++ util.py
@@ -0,0 +1,20 @@
+
+
+from typing import Dict, Sequence, Set, Text, Union
+
+
+class Util:
+	def __init__(self) -> None:
+		pass
+
+	def __str__(self) -> str:
+		return f"{self.__class__.__name__}(_)"
+
+	def __repr__(self) -> str:
+		return self.__str__()
+	
+	def __len__(self, arg: Union[Sequence, Text, Dict, Set]) -> int:
+		if (isinstance(arg, (int, float, bool))):
+			raise TypeError("Invalid argument. Only text, sequence, mapping and set are accepted")
+		else:
+			return len(arg)
