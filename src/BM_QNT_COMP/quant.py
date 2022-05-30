8542d9d9febf7cd29921b859ee960924d90d90d0
Biringa Chidera <biringachidera@gmail.com>
2022-05-30 16:17:45 -0400

    added BM_QNT_COMP directory

---
+++ src/BM_QNT_COMP/quant.py
@@ -0,0 +1,3 @@
+
+class Quant:
+	pass
--- src/SGDetector/test.py
+++ src/SGDetector/test.py
@@ -79,7 +79,3 @@ class Visualization(Test):
 		plt.yticks(fontsize = fontsize)
 		plt.tight_layout() 
 		plt.show()
-
-
-# if __name__ == "__main__":
-# 	Visualization().train_validation_loss()
